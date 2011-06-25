cf3a40be81ccf6c4d565d9ee3e59162c98e286e7
WarHead <WarHead@UWoM.de>
2011-06-25 19:52:20 +0200

    Jail:
    -----
    - trinity_string 12145 hinzugefügt
    - InGame-Ausgabe der übrigen Jailzeit gefixt
    - Konsolenausgabe gefixt
    - TimeToTimestampStr() erweitert für mehr Ausgabeformate
    - Und andere "Kleinigkeiten"...
    MIME-Version: 1.0
    Content-Type: text/plain; charset=UTF-8
    Content-Transfer-Encoding: 8bit

--- src/server/game/Miscellaneous/Language.h
+++ src/server/game/Miscellaneous/Language.h
@@ -1099,6 +1099,7 @@ enum TrinityStrings
     LANG_JAIL_INIT_3            = 12142,
     LANG_JAIL_INIT_4            = 12143,
     LANG_JAIL_FMTSTR            = 12144,
+    LANG_JAIL_DATA_LOADED       = 12145,
     LANG_JAIL_ENABLED           = 12197,
     LANG_JAIL_DISABLED          = 12198,
     LANG_JAIL_NOT_ACTIVE        = 12199
