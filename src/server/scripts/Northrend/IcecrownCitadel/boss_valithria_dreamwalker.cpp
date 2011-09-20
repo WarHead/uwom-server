f6c0f157963fa0679e1a533e29f9d95ca956d410
WarHead <WarHead@UWoM.de>
2011-09-20 07:49:18 +0200

    ICC Raid - Traumwandler: Kompilierungsfix.

--- src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_valithria_dreamwalker.cpp
@@ -1321,7 +1321,7 @@ class spell_dreamwalker_summon_suppresser : public SpellScriptLoader
                     return;
 
                 std::list<Creature*> summoners;
-                GetCreatureListWithEntryInGrid(summoners, caster, NPC_WORLD_TRIGGER, 100.0f);
+                GetCreatureListWithEntryInGrid(summoners, caster, NPC_WORLDTRIGGER, 100.0f);
                 summoners.remove_if(Trinity::UnitAuraCheck(true, SPELL_RECENTLY_SPAWNED));
                 Trinity::RandomResizeList(summoners, 2);
                 if (summoners.empty())
