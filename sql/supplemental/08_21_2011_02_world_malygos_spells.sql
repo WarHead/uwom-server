8a3685dc25471ead60f00c58ffc290b1070a7740
WarHead <WarHead@UWoM.de>
2011-09-28 01:02:37 +0200

    Malygos SQLs (aus den vorherigen Commits) wieder löschen, da diese jetzt in mein DB-SVN übernommen wurden.

--- sql/supplemental/08_21_2011_02_world_malygos_spells.sql
+++
@@ -1,9 +0,0 @@
--- SpellScripts (mostly target selection)
-DELETE FROM spell_script_names WHERE spell_id IN (56548,56397,63934,57459,61694,60936);
-INSERT INTO spell_script_names VALUES
-(56548,'spell_malygos_surge_of_power'),
-(56397,'spell_malygos_arcane_barrage'),
-(63934,'spell_malygos_arcane_barrage'),
-(57459,'spell_malygos_arcane_storm'),
-(61694,'spell_malygos_arcane_storm'),
-(60936,'spell_malygos_surge_of_power_p3');
--- sql/supplemental/08_21_2011_03_world_malygos_gameobject.sql
+++
@@ -1,2 +0,0 @@
--- remove already spawned containers
-DELETE FROM gameobject WHERE id IN (193905,193967,194158,194159);
