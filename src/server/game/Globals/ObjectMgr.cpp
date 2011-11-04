8d20f10bff964d233a3864235141aaf4a78ee125
WarHead <WarHead@UWoM.de>
2011-11-04 02:34:01 +0100

    ObjectMgr: Fehlermeldung für nicht existierende Items ausgeschaltet.

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -2344,8 +2344,8 @@ void ObjectMgr::LoadItemTemplates()
             }
 
         }
-        else
-            sLog->outErrorDb("Item (Entry: %u) does not exist in item.dbc! (not correct id?).", entry);
+        /*else
+            sLog->outErrorDb("Item (Entry: %u) does not exist in item.dbc! (not correct id?).", entry);*/
 
         if (itemTemplate.Class >= MAX_ITEM_CLASS)
         {
